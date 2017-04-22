428dc919b1fed29c636a6da118ff4704eb63f5a2
Kaz <KazWong@gmail.com>
2017-04-22 20:04:48 +0800

    Basic Setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HOME' });
+});
+
+/* GET Map. */
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'Map' });
+});
+
+/* GET 2 page. */
+router.get('/2', function(req, res, next) {
+  res.render('page', { title: '2' });
+});
+
+/* GET 3 page. */
+router.get('/3', function(req, res, next) {
+  res.render('page', { title: '3' });
 });
 
 module.exports = router;
