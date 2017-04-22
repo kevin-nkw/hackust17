69bb0af3b65611682674a7f099da1e819107ef1e
Kaz <KazWong@gmail.com>
2017-04-23 02:20:50 +0800

    Use full bootstrap lib

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components/bootstrap/dist/')));
+//app.use(express.static(path.join(__dirname, 'bower_components/bootstrap/dist/')));
+app.use(express.static(path.join(__dirname, './public/dist/')));
 
 app.use('/', index);
 app.use('/users', users);
